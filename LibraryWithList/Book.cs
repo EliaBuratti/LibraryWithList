2ea9f341e9ef1324f31348e4994ac181fc67fbd8
EliaBuratti <elia.buratti.dev@gmail.com>
2024-03-13 15:38:25 +0100

    Aggiungere i file di progetto.

---
+++ LibraryWithList/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWithList
+{
+    internal class Book
+    {
+        private Hashtable BK = new Hashtable();
+        public Book(string title, string author, string publisher, int year, int rack, int floor)
+        {
+            
+            BK.Add("Title", title);
+            BK.Add("Author", author);
+            BK.Add("Publisher", publisher);
+            BK.Add("Year", year);
+            BK.Add("Rack", rack);
+            BK.Add("Floor", floor);
+        }
+
+        public string PrintBook()
+        {
+            return "\nTitle:" + BK["Title"] + "\nAuthor:" + BK["Author"] + "\nPublisher:" + BK["Publisher"] + "\nYear:" + BK["Year"] + "\nRack:" + BK["Rack"] + BK["Floor"] + "\n";
+        }
+
+        public bool FindBookTitle(string BookTitle)
+        { 
+            return BK["Title"].ToString().Contains(BookTitle);
+        }
+
+        public bool FindAuthor(string BookAuthor)
+        {
+            return BK["Author"].ToString().Contains(BookAuthor);
+        }
+    }
+}
---
+++ LibraryWithList/Program.cs
@@ -0,0 +1,142 @@
+﻿using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Threading.Channels;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace LibraryWithList
+{
+    internal class Program
+    {
+        public static List<Book> books = new List<Book>();
+        static void Main(string[] args)
+        {
+            int choice;
+            bool inputCheck = false;
+            string BookTitle;
+            string authorName;
+            bool find = false;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Choose options:\n1 Insert book.\n2 Search Book by title.\n3 Search by author.\n4 exit.");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            AddBook();
+                            break;
+
+                        case 2:
+                            // search book by title
+                            Console.WriteLine("Type a book title:");
+                            BookTitle = Console.ReadLine().ToLower();
+
+                            foreach (Book b in books)
+                            {
+                                if (b.FindBookTitle(BookTitle))
+                                {
+                                    Console.WriteLine(b.PrintBook());
+                                    find = true;
+                                }
+                            }
+
+                            if (!find) Console.WriteLine("Book not found");
+
+                            break;
+
+                        case 3:
+                            //class search author
+
+                            Console.WriteLine("Type an author name:");
+                            authorName = Console.ReadLine().ToLower();
+
+                            foreach (Book b in books)
+                            {
+                                if (b.FindAuthor(authorName))
+                                {
+                                    Console.WriteLine(b.PrintBook());
+                                    find = true;
+                                }
+                            }
+
+                            if(!find) Console.WriteLine("Book not found");
+
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Exit.");
+                            inputCheck = true;
+                            break;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            } while (!inputCheck);
+        }
+        public static void AddBook()
+        {
+            bool inputCheck = false;
+            do
+            {
+
+                try
+                {
+
+                    Console.WriteLine("Title:");
+                    string Title = Console.ReadLine();
+
+                    Console.WriteLine("Author:");
+                    string Author = Console.ReadLine();
+
+                    Console.WriteLine("Publisher:");
+                    string Publisher = Console.ReadLine();
+
+                    Console.WriteLine("Year:");
+                    int Year = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Rack:");
+                    int Rack = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Floor:");
+                    int Floor = Convert.ToInt32(Console.ReadLine());
+
+                
+                    if (RegisterData()) books.Add(new Book(Title, Author, Publisher, Year, Rack, Floor));
+
+                        inputCheck = true;
+
+                }
+                catch (Exception e) {
+
+                    Console.WriteLine("Please insert correct data of all filed!\n" + e.Message);
+                }
+            } while (!inputCheck);
+
+        }
+
+        static public bool RegisterData()
+        {
+            bool ConfirmChoice = false;
+
+            Console.WriteLine("Do you want to save the data? (Y/N)");
+            ConfirmChoice = Console.ReadLine().ToLower() == "y" ? true : false;
+
+            if (ConfirmChoice)
+            {
+                Console.WriteLine("Book registered!");
+            }
+            else
+            {
+                Console.WriteLine("Book not registered.\n");
+            }
+
+            return ConfirmChoice;
+        }
+    }
+}
